d06e51e54970e4b7e7522e0f28d8137ddd887602
Kaleb11 <kalebtilahun29@gmail.com>
2023-04-25 16:52:05 +0300

    Modified product route

---
+++ routes/modules/product/route.js
@@ -0,0 +1,16 @@
+const BrandController = require("../../../controllers/product");
+const Category = require("../../../controllers/auth/");
+const logoutController = require("../../../controllers/Auth/LogoutController");
+const UserController = require("../../../controllers/department/userController.js");
+const validateData = require("../../../middleware/validate/module/auth/validate");
+module.exports = function (express) {
+  const route = express.Router();
+
+  route.post("/login", validateData.loginValidate, authController.loginUser);
+  route.get("/refresh/token", refreshController.refreshToken);
+  route.post("/logout", logoutController.logout);
+
+  route.post("/request-password-reset", UserController.requestPasswordReset);
+  route.post("/password-reset", UserController.resetPassword);
+  return route;
+};
