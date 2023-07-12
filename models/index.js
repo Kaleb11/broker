8817d30c16ab5921a160181add8b9078e6a2c58f
Kaleb11 <kalebtilahun29@gmail.com>
2023-07-12 16:16:09 +0300

    Added some functionality

--- models/index.js
+++ models/index.js
@@ -1,24 +1,32 @@
 "use strict";
 
 const fs = require("fs");
+const mysql2 = require("mysql2");
 const path = require("path");
 const Sequelize = require("sequelize");
-const process = require("process");
+//const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
-
+const dotenv = require('dotenv');
+dotenv.config();
 let sequelize;
+console.log("Username",config.username)
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+ sequelize = new Sequelize(
+   config.database,
+   config.username,
+  config.password,
+    {
+      dialect: 'mysql',
+      dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+      host: "db4free.net",
+      port:config.port
+    }
+  )
 }
 
 fs.readdirSync(__dirname)
